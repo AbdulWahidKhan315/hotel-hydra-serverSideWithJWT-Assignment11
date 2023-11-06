109dbcbc091b55ffb4fc9200b5c82ca06567b682
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-11-06 14:33:40 +0600

    added code for getting rooms from mongodb

--- index.js
+++ index.js
@@ -9,7 +9,49 @@ app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-q1etzcd-shard-00-00.oiitlmi.mongodb.net:27017,ac-q1etzcd-shard-00-01.oiitlmi.mongodb.net:27017,ac-q1etzcd-shard-00-02.oiitlmi.mongodb.net:27017/?ssl=true&replicaSet=atlas-q3xr8u-shard-0&authSource=admin&retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const roomsCollection = client.db('hotelHydra').collection('rooms');
+
+
+        app.get('/api/rooms',async(req,res)=>{
+            const cursor = roomsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/api/rooms_details/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await roomsCollection.findOne(query);
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
