e40b358119160164aa47dd70186c085644308a11
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-11-08 16:38:01 +0600

    added vercel.josn file for deployment in vercel

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://hotel-hydra-client.web.app','https://hotel-hydra-client.firebaseapp.com'],
     credentials: true
 }));
 app.use(express.json());
